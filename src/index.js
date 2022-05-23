7cceb8c98588366068749ee633274b54e4de9877
Troy Presley <troy.presley@gmail.com>
2022-05-22 22:49:45 -1000

    add run and processForm extras

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@
 export { default as component, ABORT } from "./component"
 export { default as collection } from "./collection"
 export { default as switchable } from "./switchable"
+export { default as processForm } from "./extra/processForm"
+export { default as run } from './extra/run'
